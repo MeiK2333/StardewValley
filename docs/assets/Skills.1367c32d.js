0a029509d9acffc7d5a55e2280f394320396ba9e
MeiK <meik2333@gmail.com>
2022-10-07 02:27:32 +0800

    修改为 hash 路由模式
    因为部署在 GitHub Pages，使用默认路由模式会导致页面刷新后 404

---
+++ docs/assets/Skills.1367c32d.js
@@ -0,0 +1,1 @@
+import{d as s,u as t,c as a,t as n,b as o,o as r}from"./index.8b03f752.js";const u=s({__name:"Skills",setup(l){const e=t();return(c,i)=>(r(),a("div",null,"Skills "+n(o(e).name),1))}});export{u as default};
